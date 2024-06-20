093a64d550bbd58c1bd8516bf53e81c30517802a
ayman-github <xdev.ayman@gmail.com>
2024-06-20 15:26:26 +0300

    just init main page, item product

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,28 @@
+import { sampleProduct } from "./data"
+import tvImg from './assets/tv.webp';
+
 function App() {
 
   return (
     <>
-      <div className='bg-green-300 h-[500px]'>
-          Home Appliances </div>
+        <header className='bg-green-300'>
+          Home Appliances  
+        </header>
+
+        <main>
+          <div className="w-full grid grid-cols-4 gap-2 p-5">
+            {
+              sampleProduct.map((product, index)=>(
+                <div key={index} className="bg-gray-200 flex flex-col justify-center">
+                  <img className="w-full" src={tvImg}/>
+                  <div className="w-full flex justify-center"> {product?.name} </div>
+                  <div className="w-full flex justify-center"> {product?.brand} </div>
+                  <div className="w-full flex justify-center"> {product?.price} </div>
+                </div>
+              ))
+            }
+          </div>
+        </main>
                   
     </>
   )
---
+++ frontend/src/types/Product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  name: string;
+  slug: string;
+  image: string;
+  category: string;
+  brand: string;
+  price: number;
+  currency: string;
+  stockCount: string;
+  descriptions: string;
+  rating: number;
+};
